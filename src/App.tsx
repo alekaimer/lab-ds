8d68c731b9f13923747f2e901d844af6d6a73fa4
Alexandre Kaimer <alekaimer@gmail.com>
2022-10-11 20:22:08 -0300

    override font sizes and color tokens

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import "./styles/global.css";
 export function App() {
   return (
     <>
-      <h1 className="font-bold text-amber-600 text-5xl">Hello world!</h1>
-      <button className="bg-violet-600 hover:bg-violet-500 text-white font-medium px-4 py-2 rounded">
+      <h1 className="font-bold text-gray-400 text-2xl">Hello world!</h1>
+      <button className="bg-cyan-500 hover:bg-cyan-300 text-white font-medium px-4 py-2 rounded">
         Enviar
       </button>
     </>
