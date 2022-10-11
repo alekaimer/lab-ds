0a74e7433802071008ec23a085abdbd0bfee7326
Alexandre Kaimer <alekaimer@gmail.com>
2022-10-11 19:49:43 -0300

    adds first code in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import "./styles/global.css";
+
 export function App() {
   return (
-    <h1>Hello world!</h1>
-  )
+    <>
+      <h1 className="font-bold text-amber-600 text-5xl">Hello world!</h1>
+      <button className="bg-violet-600 hover:bg-violet-500 text-white font-medium px-4 py-2 rounded">
+        Enviar
+      </button>
+    </>
+  );
 }
