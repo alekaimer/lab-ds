6fce1a3b0c3ef6e35cde6cee7da4a576a07b9255
Alexandre Kaimer <alekaimer@gmail.com>
2022-10-14 11:15:56 -0300

    create basic test to SignIn

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -1,9 +1,28 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { SignIn } from "./SignIn";
 
+import { within, userEvent, waitFor } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+
 export default {
   title: "Pages/SignIn",
   component: SignIn,
 } as Meta;
 
-export const Default: StoryObj = {};
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    userEvent.type(
+      canvas.getByPlaceholderText("Degite seu e-mail"),
+      "email@valido.com"
+    );
+    userEvent.type(canvas.getByPlaceholderText("******"), "12345678");
+
+    userEvent.click(canvas.getByRole("button"));
+
+    await waitFor(() =>
+      expect(canvas.getByText("Login realizado!")).toBeInTheDocument()
+    );
+  },
+};
